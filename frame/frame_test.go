e0bc13a91f41f2fcee651f8d5349645dfd9f48b9
Alexander Guy <alexander@andern.org>
2014-08-14 16:12:47 -0700

    Use the new API, added another test.

--- frame/frame_test.go
+++ frame/frame_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"strings"
 	"bufio"
+	"bytes"
 )
 
 func _F(s string) string {
@@ -37,17 +38,17 @@ host:localhost
 		t.FailNow()
 	}
 
-	if f.cmd != "CONNECT" {
+	if f.Cmd != "CONNECT" {
 		t.Error("incorrect command parsed")
 	}
 
 	failedHeaders := false
 
-	if len(f.headers["accept-version"]) != 1 || f.headers["accept-version"][0] != "1.2" {
+	if len(f.Headers["accept-version"]) != 1 || f.Headers["accept-version"][0] != "1.2" {
 		failedHeaders = true
 	}
 
-	if len(f.headers["host"]) != 1 || f.headers["host"][0] != "localhost" {
+	if len(f.Headers["host"]) != 1 || f.Headers["host"][0] != "localhost" {
 		failedHeaders = true
 	}
 
@@ -56,12 +57,55 @@ host:localhost
 	}
 
 
-	if !f.complete {
+	if !f.Complete {
 		t.Error("failed to properly parse body.")
 	} else {
-		if len(f.body) != 0 {
+		if len(f.Body) != 0 {
 			t.Error("body should have been empty but isn't.")
 		}
 	}
 }
 
+
+func TestSend1(t *testing.T) {
+	r := _FR(_N(`SEND
+destination:/queue/a
+content-type:text/plain
+
+hello queue a
+`))
+	f, err := NewFrameFromReader(r)
+	if err != nil {
+		t.Error("the test frame didn't parse", err)
+		t.FailNow()
+	}
+
+	if f == nil {
+		t.Error("test frame wasn't allocated")
+		t.FailNow()
+	}
+
+	if f.Cmd != "SEND" {
+		t.Error("command was parsed incorrectly.")
+	}
+
+	if len(f.Headers) != 2 {
+		t.Error("we parsed an incorrect number of headers.")
+	}
+
+	if len(f.Headers["destination"]) != 1 || f.Headers["destination"][0] != "/queue/a" {
+		t.Error("we parsed destination wrong.")
+	}
+
+	if len(f.Headers["content-type"]) != 1 || f.Headers["content-type"][0] != "text/plain" {
+		t.Error("we parsed content-type wrong.")
+	}
+
+	if !f.Complete {
+		t.Error("we didn't find a complete frame.")
+	}
+
+	if bytes.Compare(f.Body, []byte("hello queue a\r\n")) != 0 {
+		t.Error("we didn't parse the body correctly.")
+	}
+}
